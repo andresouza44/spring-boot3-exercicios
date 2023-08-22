af12c4a6d1b10357709fe900977584614915f0da
Andre M. de Souza <andresouza44@gmail.com>
2023-08-22 11:26:30 -0300

    user and transaction class

---
+++ pickpaysimplificado/src/main/java/domain/transaction/Transaction.java
@@ -0,0 +1,41 @@
+package domain.transaction;
+
+
+import domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long Id;
+    BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name="sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name="receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+
+
+
+
+}
