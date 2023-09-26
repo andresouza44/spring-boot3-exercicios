2feda1137a42f89924fe045124ec946d29ce8fd0
Andre M. de Souza <andresouza44@gmail.com>
2023-09-26 13:34:10 -0300

    criado o header e o footer

---
+++ Employee-fullstack/frontend-employee/src/components/FooterComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span className='text'>Created 2023</span>
+        </footer>        
+    </div>
+  )
+}
+export default FooterComponent
