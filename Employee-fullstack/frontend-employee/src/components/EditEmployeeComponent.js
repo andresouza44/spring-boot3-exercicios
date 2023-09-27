93644de151adb5201cac4d4056d920d5d8dda21d
Andre M. de Souza <andresouza44@gmail.com>
2023-09-27 13:17:41 -0300

    correções no editEmployee

--- Employee-fullstack/frontend-employee/src/components/EditEmployeeComponent.js
+++ Employee-fullstack/frontend-employee/src/components/EditEmployeeComponent.js
@@ -24,10 +24,10 @@ useEffect(() => {
 }, []);
 
 
-const EditEmployee = async (e) => {
+const onSubmit = async (e) => {
   e.preventDefault();
-
-  await axios.put("http://localhost:8080/employee",employee)
+  await axios.put(`http://localhost:8080/employee/${id}`,employee)
+  navigate("/")
 }
 
 const loadUser = async () => {
@@ -40,6 +40,48 @@ const loadUser = async () => {
     <div className='row'>
       <div className='col-md-6 offset-md-3 border rounde p-4 mt2 shadow'>
         <h2 className='text-center m-4'>Edit Employee</h2>
+        <form onSubmit={(e) => onSubmit(e)}>
+          <div className='mb-3'>
+              <label htmlFor='firstName' className='form-label'>First Name</label>
+              <input
+                type={"text"}
+                className='form-control'
+                placeholder='Enter your First Name'
+                name="firstName"
+                value={firstName}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+          <div className='mb-3'>
+            <label htmlFor='lastName' className='form-label'>Last Name</label>
+            <input
+              type={"text"}
+              className='form-control'
+              placeholder='Enter your Last Name'
+              name="lastName"
+              value={lastName}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <div className='mb-3'>
+            <label htmlFor='emailIde' className='form-label'>Email</label>
+            <input
+              type={"email"}
+              className='form-control'
+              placeholder='Enter yourEmail'
+              name="emailId"
+              value={emailId}
+              onChange={(e) => onInputChange(e)}
+            />
+          </div>
+          <button type='submit' className='btn btn-outline-primary'>
+                        Submit
+                    </button>
+                    <Link className='btn btn-outline-danger mx-2' to="/">
+                        Cancel
+                    </Link>
+          
+        </form>
 
       </div>
 
