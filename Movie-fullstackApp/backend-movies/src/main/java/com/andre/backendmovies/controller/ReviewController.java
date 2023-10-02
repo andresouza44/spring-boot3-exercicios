4e2eef9944bef143d5cd2b1fa4b15c4dfce19d15
Andre M. de Souza <andresouza44@gmail.com>
2023-10-02 18:37:50 -0300

    cross origin

--- Movie-fullstackApp/backend-movies/src/main/java/com/andre/backendmovies/controller/ReviewController.java
+++ Movie-fullstackApp/backend-movies/src/main/java/com/andre/backendmovies/controller/ReviewController.java
@@ -6,15 +6,13 @@ import com.andre.backendmovies.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping(value="/api/v1/review")
+@CrossOrigin("*")
 public class ReviewController {
 
     @Autowired
